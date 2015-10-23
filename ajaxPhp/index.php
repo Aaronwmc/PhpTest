c91e51c9920f94aa5d6d094a36dfcccf2382e4f1
Aaronwmc <18998474944@163.com>
2015-10-23 22:39:15 +0800

    update

--- ajaxPhp/index.php
+++ ajaxPhp/index.php
@@ -2,27 +2,27 @@
 <script>
 function showHint(str)
 {
-	if (str.length==0)
+	if(str.length==0)
 	{
-		document.getElementById("txtHint").innerHTML="";
-		return;
+		document.getElementById('txtHint').innerHTML='';
+		return ;
 	}
-	if (window.XMLHttpRequest)
-	{// code for IE7+, Firefox, Chrome, Opera, Safari
+	if(window.XMLHttpRequest)
+	{
 		xmlhttp=new XMLHttpRequest();
 	}
-	else
-	{// code for IE6, IE5
-		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	else 
+	{
+		xmlhttp=new ActiveXObject('Microsoft.XMLHTTP');
 	}
 	xmlhttp.onreadystatechange=function()
 	{
-		if (xmlhttp.readyState==4 && xmlhttp.status==200)
+		if(xmlhttp.readyState==4&&xmlhttp.status==200)
 		{
-			document.getElementById("txtHint").innerHTML=xmlhttp.responseText;
+			document.getElementById('txtHint').innerHTML=xmlhttp.responseText;
 		}
 	}
-	xmlhttp.open("GET","gethint.php?q="+str,true);
+	xmlhttp.open('get','gethint.php?q='+str,true);
 	xmlhttp.send();
 }
 </script>
---
+++ ajaxPhp/test.php
@@ -0,0 +1,35 @@
+<?php header('Content-Type:text/html;charset=utf-8');?>
+<script type="text/javascript">
+	function showHint(str)
+	{	
+		if(str.length==0)
+		{
+			document.getElementById('txtHint').innerHTML='';
+			return ;
+		}
+		if(window.XMLHttpRequest)
+		{
+			xmlhttp=new XMLHttpRequest();
+		}
+		else
+		{
+			xmlhttp=new ActiveXObject('Microsoft.XMLHTTP');
+		}
+		xmlhttp.onreadystatechange=function()
+		{
+			if(xmlhttp.readyState==4&&xmlhttp.status==200)
+			{
+				document.getElementById('txtHint').innerHTML=xmlhttp.responseText;
+			}
+		}
+		xmlhttp.open('get','gethint.php?q='+str,true);
+		xmlhttp.send();
+	}
+</script>
+
+
+<p><b>在输入框中输入一个姓名:</b></p>
+<form>
+姓名: <input type="text" onkeyup="showHint(this.value)">
+</form>
+<p>返回值: <span id="txtHint"></span></p>
