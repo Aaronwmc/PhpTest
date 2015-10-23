378d236bc7f78b1954d2d1a2648f7ff1303f917f
Aaronwmc <18998474944@163.com>
2015-10-23 15:42:24 +0800

    update

--- ajaxPhp/gethint.php
+++ ajaxPhp/gethint.php
@@ -38,32 +38,32 @@ $q=$_GET["q"];
 //查找是否由匹配值， 如果 q>0
 if (strlen($q) > 0) 
 {
-$hint="";
-for($i=0; $i<count($a); $i++)
-{
-if (strtolower($q)==strtolower(substr($a[$i],0,strlen($q))))
-{
-if ($hint=="")
-{
-$hint=$a[$i];
-}
-else
-{
-$hint=$hint." , ".$a[$i];
-}
-}
-}
+	$hint="";
+	for($i=0; $i<count($a); $i++)
+	{
+		if (strtolower($q)==strtolower(substr($a[$i],0,strlen($q))))
+		{
+			if ($hint=="")
+			{
+				$hint=$a[$i];
+			}
+			else
+			{
+				$hint=$hint." , ".$a[$i];
+			}
+		}
+	}
 }
 
 // 如果没有匹配值设置输出为 "no suggestion"
 // or to the correct values
 if ($hint == "")
 {
-$response="no suggestion";
+	$response="no suggestion";
 }
 else
 {
-$response=$hint;
+	$response=$hint;
 }
 
 //输出返回值
